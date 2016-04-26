025f491efd7dbea91df147ed5fccd6f7d9be5e24
Wagner Galdino de Carvalho <wagalwagal@gmail.com>
2016-04-26 20:21:14 -0300

    Update leap.go

--- dojo/leap.go
+++ dojo/leap.go
@@ -1,8 +1,5 @@
 package leap
 
-// TestVersion is the version test suite the program is designed to pass.
-const TestVersion = 1
-
 /*
 IsLeapYear determines if a year is a leap year according the following rule:
 Leap years are evenly divisble by 4 and not by 100
